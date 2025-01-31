209effbc19b4653734c9ebee7d802c5e5174e7b8
raushan563 <152180033+raushan563@users.noreply.github.com>
2025-01-31 22:58:03 +0530

    base

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import { FiMenu } from "react-icons/fi";
+import { IoSearch } from "react-icons/io5";
 
 const Sidebar = () => {
     const [isCollapsed, setIsCollapsed] = useState(false);
 
     return (
-        <div className={`h-full ${isCollapsed ? 'w-16' : 'w-[20%] bg-gray-200'}  rounded-xl transition-all duration-500 overflow-hidden`}>
-            <div className='flex p-4 justify-between items-center'>
+        <div className={`h-full p-4 ${isCollapsed ? 'w-16' : 'w-[20%] bg-gray-200'}  rounded-xl transition-all duration-500 overflow-hidden`}>
+            <div className='flex justify-between items-center'>
                 {!isCollapsed && <h1 className='text-2xl font-bold font-sans whitespace-nowrap'>Menu</h1>}
                 <FiMenu
                     className='text-2xl font-bold cursor-pointer'
                     onClick={() => setIsCollapsed(!isCollapsed)}
                 />
+            </div>
+            <div className='bg-transparent w-full flex justify-evenly border border-gray-400'>
+            <IoSearch className='text-2xl '/>
+                <input type="text" placeholder='Search' className='outline-none'/>
             </div>
         </div>
     );
