3924796c73b763cbd6d2d7bdedec1554fc573381
raushan563 <152180033+raushan563@users.noreply.github.com>
2025-02-01 15:37:28 +0530

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,13 +6,17 @@ import { PiListChecks } from "react-icons/pi";
 import { SlCalender } from "react-icons/sl";
 import { FaNoteSticky } from "react-icons/fa6";
 import { IoAddOutline } from "react-icons/io5";
+import { FaSignOutAlt } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
     const [isCollapsed, setIsCollapsed] = useState(false);
 
-    const handleLogout = () => {
+    const navigate = useNavigate();
+
+    const logout = () => {
         localStorage.removeItem('authToken');
-        window.location.href = "/login"; // Redirect to login page
+        navigate('/login')
     };
 
 
@@ -32,7 +36,7 @@ const Sidebar = () => {
             {!isCollapsed && <h1 className='text-gray-700 font-bold text-xs mt-3'>TASKS</h1>}
 
 
-            {!isCollapsed && <div className='Upcomming flex mt-3'>
+            {!isCollapsed && <div className='Upcomming cursor-pointer flex mt-3'>
                 <MdKeyboardDoubleArrowRight className='text-gray-700 text-xl font-bold mt-1' />
                 <h2 className='px-3 text-gray-600 font-medium'>Upcoming</h2>
                 <div className='ml-30 w-5 rounded-md mt-1 flex justify-center items-center bg-gray-300'>
@@ -41,7 +45,7 @@ const Sidebar = () => {
 
             </div>
             }
-            {!isCollapsed && <div className='TODAY flex mt-3'>
+            {!isCollapsed && <div className='TODAY cursor-pointer flex mt-3'>
                 <PiListChecks className='text-gray-700 text-2xl font-bold mt-1' />
                 <h2 className='px-3 text-gray-600 font-medium'>Today</h2>
                 <div className='ml-37 w-5 rounded-md mt-1 flex justify-center items-center bg-white'>
@@ -50,7 +54,7 @@ const Sidebar = () => {
 
             </div>}
 
-            {!isCollapsed && <div className='CALENDAR flex mt-3'>
+            {!isCollapsed && <div className='CALENDAR cursor-pointer flex mt-3'>
                 <SlCalender className='text-gray-700 text-xl font-bold mt-1' />
                 <h2 className='px-3 text-gray-600 font-medium'>Calendar</h2>
                 <div className='ml-33 w-5 rounded-md mt-1 flex justify-center items-center bg-gray-300'>
@@ -59,7 +63,7 @@ const Sidebar = () => {
 
             </div>}
 
-            {!isCollapsed && <div className='STICKY WALL flex mt-3'>
+            {!isCollapsed && <div className='STICKY WALL cursor-pointer flex mt-3'>
                 <FaNoteSticky className='text-gray-700 text-xl font-bold mt-1' />
                 <h2 className='px-3 text-gray-600 font-medium'>Sticky Wall</h2>
                 <div className='ml-30 w-5 rounded-md mt-1 flex justify-center items-center bg-gray-300'>
@@ -71,7 +75,7 @@ const Sidebar = () => {
             {!isCollapsed && <div className=' LISTSDIV mt-3 flex flex-col w-full  '>
                 <h1 className='text-gray-700 font-bold text-sm mt-2'>LISTS</h1>
 
-                <div className=' px-2 py-4 w-full h-4 flex items-center   mt-3 rounded-md'>
+                <div className=' px-2 py-4 w-full h-4 flex items-center cursor-pointer  mt-3 rounded-md'>
                     <div className=' h-4 w-4 bg-red-700 rounded-md  '></div>
                     <h3 className='px-5 text-gray-600 font-medium'>Personal</h3>
                     <div className=' ml-25 h-4 w-7 rounded-md  flex justify-center items-center bg-gray-300'>
@@ -79,7 +83,7 @@ const Sidebar = () => {
                     </div>
                 </div>
 
-                <div className=' px-2 py-4 w-full h-4 flex items-center   mt-3 rounded-md'>
+                <div className=' px-2 py-4 w-full h-4 flex items-center cursor-pointer  mt-3 rounded-md'>
                     <div className=' h-4 w-4 bg-sky-400 rounded-md  '></div>
                     <h3 className='px-5 text-gray-600 font-medium'>Work</h3>
                     <div className=' ml-31 h-4 w-7 rounded-md  flex justify-center items-center bg-gray-300'>
@@ -87,7 +91,7 @@ const Sidebar = () => {
                     </div>
                 </div>
 
-                <div className=' px-2 py-4 w-full h-4 flex items-center   mt-3 rounded-md'>
+                <div className=' px-2 py-4 w-full h-4 flex items-center  cursor-pointer mt-3 rounded-md'>
                     <div className=' h-4 w-4 bg-yellow-300 rounded-md  '></div>
                     <h3 className='px-5 text-gray-600 font-medium'>List 1</h3>
                     <div className=' ml-32 h-4 w-7 rounded-md  flex justify-center items-center bg-gray-300'>
@@ -95,14 +99,37 @@ const Sidebar = () => {
                     </div>
                 </div>
 
-                <div className='CALENDAR flex mt-3'>
+                <div className=' cursor-pointer flex mt-3'>
                     <IoAddOutline className='text-gray-700 text-xl font-bold mt-1' />
                     <h2 className='px-4 text-gray-600 font-semibold'>Add New List</h2>
                 </div>
             </div>}
 
-            <hr className='mt-4 text-gray-100'></hr>
+            {!isCollapsed && <hr className='mt-4 text-gray-100'></hr>}
+            {!isCollapsed && <h1 className='text-gray-700 font-bold text-sm mt-2'>TAGS</h1>}
+            {!isCollapsed && <div className='w-full flex gap-3 '>
+                <div className='px-2 py-0.5 mt-1 cursor-pointer bg-blue-200 rounded-md flex items-center'>
+                     <h1 className='font-semibold text-gray-600'>Tag 1</h1>
+                </div>
 
+                <div className='px-2 py-0.5 mt-1 cursor-pointer bg-pink-400 rounded-md flex items-center'>
+                     <h1 className='font-semibold text-gray-600'>Tag 2</h1>
+                </div> 
+
+                <div className='px-2 py-0.5 mt-1 cursor-pointer bg-gray-300 rounded-md  flex items-center'>
+                     <h1 className='font-semibold flex items-center justify-center text-gray-600'> 
+                     <IoAddOutline className='text-gray-700 text-xl font-bold ' />
+                        Add Tag</h1>
+                </div>
+
+            </div>}
+           
+            {!isCollapsed && <div onClick={logout} className='CALENDAR cursor-pointer flex mt-20'>
+                <FaSignOutAlt  className='text-gray-700 text-xl font-bold mt-1' />
+                <h2 className='px-3 text-gray-600 font-medium'>Sign Out</h2>
+                
+
+            </div>}
 
         </div>
     );
